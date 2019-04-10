b7a66c3d83183106fadce567ddf6ad196c8ba5bf
PROMAR <PROMAR@example.com>
2019-04-09 23:41:23 -0400

    delete unused pictures and add TensorFlow test

--- src/main/java/com/example/promar/Main.java
+++ src/main/java/com/example/promar/Main.java
@@ -1,21 +1,47 @@
 package com.example.promar;
 
+import com.example.promar.model.BoxPosition;
 import com.example.promar.model.DescriptorType;
 import com.example.promar.model.ImageFeature;
+import com.example.promar.model.Recognition;
 import com.example.promar.util.ImageUtil;
 import org.opencv.core.*;
 import org.opencv.features2d.Features2d;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.List;
+
 public class Main {
-    static int DEBUG = 0;
 
     public static void main(String[] args) throws Exception {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        testTensorFlow();
         testPROMAR();
     }
 
-    static void testPROMAR(){
-        String path = "src/main/resources/image/single_distortion/furry_bear/";
+    static void testTensorFlow() {
+        String imgPath = "src/main/resources/image/dog_cat.jpg";
+        BufferedImage img = ImageUtil.loadImage(imgPath);
+
+        ObjectDetector objectDetector = new ObjectDetector();
+        objectDetector.init();
+        List<Recognition> recognitions = objectDetector.recognizeImage(img);
+
+        Graphics2D graphics = (Graphics2D) img.getGraphics();
+        for (Recognition r : recognitions) {
+            //get scaled box location
+            BoxPosition bp = r.getScaledLocation((float)img.getWidth()/r.getModelSize(), (float)img.getHeight()/r.getModelSize());
+            graphics.drawString(r.getTitle() + " " + r.getConfidence(), bp.getLeft(), bp.getTop() - 7);
+            // draw bounding box
+            graphics.drawRect(bp.getLeftInt(), bp.getTopInt(), bp.getWidthInt(), bp.getHeightInt());
+        }
+        //display recognized result
+        ImageUtil.displayImage(img);
+    }
+
+    static void testPROMAR() {
+        String path = "src/main/resources/image/furry_bear/";
         Mat t_img = ImageUtil.loadMatImage(path+"0.png");
 //        Mat q_img = ImageUtil.loadMatImage(path+"20.png");
         Mat q_img = ImageUtil.loadMatImage(path+"false/20.jpg");
